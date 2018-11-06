a52649a7a0e3c586ef69733bcd64c72a2d161983
alejodosr <alejandro.rramos@alumnos.upm.es>
2018-11-06 14:49:59 -0500

    modified to be initialized in a certain epoch

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -408,6 +408,8 @@ def parse_args(args):
     parser.add_argument('--image-max-side',   help='Rescale the image if the largest side is larger than max_side.', type=int, default=1333)
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
     parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
+    parser.add_argument('--initial_epoch',    help='Set the epoch to be trained at the starting point', type=int, default=0)
+
 
     return check_args(parser.parse_args(args))
 
@@ -486,6 +488,7 @@ def main(args=None):
         epochs=args.epochs,
         verbose=1,
         callbacks=callbacks,
+        initial_epoch=args.initial_epoch,
     )
 
 
