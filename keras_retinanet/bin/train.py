5d1729a2df218c1f6eb1dc3f9f6fa0f24b28a007
alejodosr <alejandro.rramos@alumnos.upm.es>
2018-11-14 14:22:22 -0500

    fix argument

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -419,7 +419,7 @@ def parse_args(args):
     parser.add_argument('--config',           help='Path to a configuration parameters .ini file.')
     parser.add_argument('--weighted-average', help='Compute the mAP using the weighted average of precisions among classes.', action='store_true')
     parser.add_argument('--initial_epoch',    help='Set the epoch to be trained at the starting point', type=int, default=0)
-    parser.add_argument('--use_tpu',          help='Set the usage of TPU for training', type=int, default=0)
+    parser.add_argument('--use_tpu',          help='Set the usage of TPU for training', default=False)
     parser.add_argument('--tpu_address',      help='Set the usage of TPU address for training', type=str, default='')
 
     return check_args(parser.parse_args(args))
